1d09066d72c5a53702708518b905789b159e1ecf
Christopher Klapp <christopher@klapp.name>
2020-01-10 10:22:23 -0500

    Updates

--- build/bundle.105de5e2.js
+++ build/bundle.105de5e2.js
@@ -57,7 +57,7 @@
 /******/ 	// undefined = chunk not loaded, null = chunk preloaded/prefetched
 /******/ 	// Promise = chunk loading, 0 = chunk loaded
 /******/ 	var installedChunks = {
-/******/ 		"runtime~main": 0
+/******/ 		"runtime-main": 0
 /******/ 	};
 /******/
 /******/ 	var deferredModules = [];
